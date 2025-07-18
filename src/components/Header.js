261bd8739618dbcfffd473526692d03b48482095
jared kiprop <jaredassociates@gmail.com>
2025-07-18 08:39:17 +0300

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 function Header({ isDarkMode, onDarkModeClick }) {
+  const buttonText = isDarkMode ? "Light Mode" : "Dark Mode";
+
   return (
     <header>
-      <h2>Shopster</h2>
-      <button onClick={onDarkModeClick}>
-        {isDarkMode ? "Dark" : "Light"} Mode
-      </button>
+      <h1>Shopping List</h1>
+      <button onClick={onDarkModeClick}>{buttonText}</button>
     </header>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,47 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newItem = {
+      name,
+      category,
+      isInCart: false,
+    };
+
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newItem),
+    })
+      .then((r) => r.json())
+      .then(onAddItem);
+
+    setName("");
+    setCategory("Produce");
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
+          aria-label="Name"
           type="text"
           name="name"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
       </label>
-
       <label>
         Category:
         <select
+          aria-label="Category"
           name="category"
           value={category}
           onChange={(e) => setCategory(e.target.value)}
@@ -26,9 +49,9 @@ function ItemForm() {
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
+          <option value="Meat">Meat</option>
         </select>
       </label>
-
       <button type="submit">Add to List</button>
     </form>
   );
